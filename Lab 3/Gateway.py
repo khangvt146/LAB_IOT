80998c4c594358bdb83828c6a50ec4359e10833c
khangvt146 <khangvotan@gmail.com>
2022-04-23 16:15:33 +0700

    Updated

--- Lab 3/Gateway.py
+++ Lab 3/Gateway.py
@@ -1,17 +1,34 @@
+print("IoT Gateway")
 import paho.mqtt.client as mqttclient
 import time
 import json
-
 import serial.tools.list_ports
-
-mess = ""
-bbc_port = "COM6"
-if len(bbc_port) > 0:
-    ser = serial.Serial(port=bbc_port, baudrate=115200)
 
 BROKER_ADDRESS = "demo.thingsboard.io"
 PORT = 1883
-THINGS_BOARD_ACCESS_TOKEN = "kvOBWfYtgAYJ9E6TV0hn"
+THINGS_BOARD_ACCESS_TOKEN = "XaMmHHomIt3wIqDrmqM2"
+mess = ""
+
+
+# TODO: Add your token and your comport
+
+def getPort():
+    ports = serial.tools.list_ports.comports()
+    N = len(ports)
+    commPort = "None"
+    for i in range(0, N):
+        port = ports[i]
+        strPort = str(port)
+        if "USB Serial Device" in strPort:
+            splitPort = strPort.split(" ")
+            commPort = (splitPort[0])
+    return commPort
+
+
+# Please check the comport in the device manager
+bbc_port = getPort()
+if len(bbc_port) > 0:
+    ser = serial.Serial(port=bbc_port, baudrate=115200)
 
 
 def processData(data):
@@ -19,7 +36,13 @@ def processData(data):
     data = data.replace("#", "")
     splitData = data.split(":")
     print(splitData)
-    #TO DO
+    collect_data = {}
+    # TODO: Add your source code to publish data to the server
+    if splitData[1] == 'TEMP':
+        collect_data = {'temperature': splitData[2]}
+    if splitData[1] == 'LIGHT':
+        collect_data = {'light': splitData[2]}
+    client.publish('v1/devices/me/telemetry', json.dumps(collect_data), 1)
 
 
 def readSerial():
@@ -44,21 +67,29 @@ def subscribed(client, userdata, mid, granted_qos):
 def recv_message(client, userdata, message):
     print("Received: ", message.payload.decode("utf-8"))
     temp_data = {'value': True}
+    cmd = 0
+    # TODO: Update the cmd to control 2 devices
     try:
         jsonobj = json.loads(message.payload)
-        # if jsonobj['method'] == "setValue":
-        if jsonobj['method'] == "setLED":
-            temp_data['value_LED'] = jsonobj['params']
+        if jsonobj['method'] == "setLed":
+            temp_data['value'] = jsonobj['params']
             client.publish('v1/devices/me/attributes', json.dumps(temp_data), 1)
-        if jsonobj['method'] == "setPump":
-            temp_data['value_PUMP'] = jsonobj['params']
+            if jsonobj['params']:
+                cmd = 'led-on'
+            else:
+                cmd = 'led-off'
+        if jsonobj['method'] == "setFan":
+            temp_data['value'] = jsonobj['params']
             client.publish('v1/devices/me/attributes', json.dumps(temp_data), 1)
-
+            if jsonobj['params']:
+                cmd = 'fan-on'
+            else:
+                cmd = 'fan-off'
     except:
         pass
 
     if len(bbc_port) > 0:
-        ser.write((str("khangne") + "#").encode())
+        ser.write((str(cmd) + "#").encode())
 
 
 def connected(client, usedata, flags, rc):
@@ -79,18 +110,9 @@ client.loop_start()
 client.on_subscribe = subscribed
 client.on_message = recv_message
 
-temp = 30
-humi = 50
-light_intensity = 100
-counter = 0
-
-longitude = 108.446756
-latitude = 11.9536685
 while True:
-    # if len(bbc_port) > 0:
-    #     readSerial()
-    collect_data = {'temperature': temp, 'light': humi}
-    client.publish('v1/devices/me/attributes', json.dumps(collect_data), 1)
-    temp += 1
-    humi += 1
-    time.sleep(20)
+    if len(bbc_port) > 0:
+        readSerial()
+
+    time.sleep(1)
+
